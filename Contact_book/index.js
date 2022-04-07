9557c92c66dd89a7076d29acab99059d9a178eb6
anjiwon319 <anjiwon319@kookmin.ac.kr>
2022-04-07 13:13:11 +0900

    Feat: 추가 DB find, create 함수

--- Contact_book/index.js
+++ Contact_book/index.js
@@ -16,6 +16,45 @@ db.on('error', function(err){
 // Other settings
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// DB schema
+var contactSchema = mongoose.Schema({
+  name:{type:String, required:true, unique:true},
+  email:{type:String},
+  phoneNum:{type:String}
+});
+
+// db에 contact라는 이름의 collection 연결, 없다면 생성 후 연결
+var Contact = mongoose.model('contact', contactSchema);
+
+// Routes - Home
+app.get('/', function(req, res){
+  res.redirect('/contacts'); //메인 페이지가 /contacts 페이지
+});
+
+// Contacts - Index - find : db에 검색 조건에 맞는 data 서치 ({} : 검색 조건 비어있음, 모든 데이터 return)
+app.get('/contacts', function(req, res){
+  Contact.find({}, function(err, contacts){
+    if(err) return res.json(err);
+    res.render('contacts/index', {contacts:contacts}); 
+    //render: 템플릿(html, ejs)을 불러옴, redirect: url로 이동
+  });
+});
+
+// Contacts - New
+app.get('/contacts/new', function(req, res){
+  res.render('contacts/new');
+});
+
+// Contacts - create : db에 data 생성
+app.post('/contacts', function(req, res){
+  Contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
 
 // Port setting
 var port = 3000;
